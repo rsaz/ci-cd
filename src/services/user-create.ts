286f63f6bc18233e3e7149ef7696127be6908798
Richard Zampieri <br.developer@gmail.com>
2023-08-28 22:34:21 -0700

    update ci-cd

--- src/services/user-create.ts
+++ src/services/user-create.ts
@@ -1,6 +1,7 @@
 import e from "express";
 import { User } from "../models/user";
 
+// Method to crate user
 function userCreate({ name, email, password}: User): User {
     let user: User = new User();
     user.name = name;
