638219d94f00b4d058ea66e0930703f3a61bbb37
Richard Zampieri <br.developer@gmail.com>
2023-08-28 21:34:20 -0700

    add os pipeline

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
     testEnvironment: "node",
     testMatch: ["**/dist/test/**/*.test.js"],
     testPathIgnorePatterns: ["/node_modules/"],
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageReporters: ["json", "text", "html"],
 };
